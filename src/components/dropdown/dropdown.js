2fcb907024682066da522c7821690c8b12e2e441
QumiQumi <43902909+QumiQumi@users.noreply.github.com>
2020-02-10 19:01:13 +0700

    это к предыдущему коммиту

--- src/components/dropdown/dropdown.js
+++
@@ -1,8 +0,0 @@
-// var dropdown = document.getElementsByid('dropdown');
-// //var dropdownAfter = window.getComputedStyle(dropdown, '::after');
-// dropdown.addEventListener("click", () => dropdown.focus());
-$(document).ready(function() {
-    $('#dropdown').click(function() {
-        $('#dropdown').select();
-    });
-})
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import './main.scss';
-//import './components/dropdown/dropdown'
+import './components/dropdown/dropdown';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+
 console.log('Hello main.js!')
