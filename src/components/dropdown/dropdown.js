7497eb3a7743de09b79c34cdb41032c45c221a6f
QumiQumi <43902909+QumiQumi@users.noreply.github.com>
2020-02-11 23:51:08 +0700

    Убрал нафиг все плагины, решил сам пытаться писать, посмотрим на сколько хватит.

---
+++ src/components/dropdown/dropdown.js
@@ -0,0 +1,10 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     drop = document.getElementsByClassName('dropdown__name');
+//     let i = 0;
+//     while (i < drop.length) {
+//         drop[i].addEventListener('click', function() {
+//             event.target.closest(".dropdown").classList.toggle("active");
+//         })
+//         i++;
+//     }
+// })
--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
 import './main.scss';
 import './jquery-glob';
-//import './components/text-field/text-field';
-//import './components/drop-down/drop-down';
-//import './components/i-q-dropdown/i-q-dropdown';
-//import '../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.js';
+import './components/text-field/text-field';
+import './components/dropdown/dropdown';
 
-// function requireAll(requireContext) {
-//     return requireContext.keys().map(requireContext);
-// }
-// requireAll(require.context('./components', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/)); // pattern to take each .js(x) files except of the ones with __tests__ directory https://regex101.com/r/J8NWTj/1
-//requireAll(require.context('./pages', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
+
 
 console.log('Hello main.js!')
