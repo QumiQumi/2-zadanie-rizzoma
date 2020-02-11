9df8da990f5c8d380515ffd80d63197d03bf0ba1
QumiQumi <43902909+QumiQumi@users.noreply.github.com>
2020-02-11 21:42:11 +0700

    Переделал структуру проекта

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import './main.scss';
-import './components/dropdown/dropdown';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import './jquery-glob';
+//import './components/text-field/text-field';
+//import './components/drop-down/drop-down';
+//import './components/i-q-dropdown/i-q-dropdown';
+//import '../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.js';
+
+// function requireAll(requireContext) {
+//     return requireContext.keys().map(requireContext);
+// }
+// requireAll(require.context('./components', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/)); // pattern to take each .js(x) files except of the ones with __tests__ directory https://regex101.com/r/J8NWTj/1
+//requireAll(require.context('./pages', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
 
 console.log('Hello main.js!')
