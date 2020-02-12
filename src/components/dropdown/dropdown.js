140c3d2a5ce8ea9d5ff41ff74230d39bccc3a67a
QumiQumi <43902909+QumiQumi@users.noreply.github.com>
2020-02-12 13:28:56 +0700

    Сделал, чтобы dropdown выдвигался

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,10 +1,14 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     drop = document.getElementsByClassName('dropdown__name');
-//     let i = 0;
-//     while (i < drop.length) {
-//         drop[i].addEventListener('click', function() {
-//             event.target.closest(".dropdown").classList.toggle("active");
-//         })
-//         i++;
-//     }
-// })
+//import './dropdown.scss';
+
+console.log('dropdown');
+
+document.addEventListener('DOMContentLoaded', function() {
+    drop = document.getElementsByClassName('dropdown__name');
+    let i = 0;
+    while (i < drop.length) {
+        drop[i].addEventListener('click', function() {
+            event.target.closest(".dropdown").classList.toggle("active");
+        })
+        i++;
+    }
+})
--- src/main.js
+++ src/main.js
@@ -2,7 +2,4 @@ import './main.scss';
 import './jquery-glob';
 import './components/text-field/text-field';
 import './components/dropdown/dropdown';
-
-
-
 console.log('Hello main.js!')
